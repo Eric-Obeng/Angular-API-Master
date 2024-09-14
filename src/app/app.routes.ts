35dedad72fa1f7e7e04dcc7234d1c6f66cc9c55b
Eric-Obeng <obengeric.3958@gmail.com>
2024-09-14 16:06:10 +0000

    chore: router configured

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { PostListComponent } from './components/post-list/post-list.component';
+import { PostDetailsComponent } from './components/post-details/post-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/posts',
+    pathMatch: 'full',
+  },
+  {
+    path: 'posts',
+    component: PostListComponent,
+  },
+  {
+    path: 'post-details/:id',
+    component: PostDetailsComponent,
+  },
+  // { path: '**', redirectTo: '/posts' },
+];
