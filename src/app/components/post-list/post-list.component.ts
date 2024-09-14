724d4ca2507b1fc7ae7634e80f988283ca0e713d
Eric-Obeng <obengeric.3958@gmail.com>
2024-09-14 16:09:47 +0000

    feat: post list component to list all the post

---
+++ src/app/components/post-list/post-list.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { Data } from '../../interfaces/data';
+import { CommentService } from '../../services/comment.service';
+import { DataService } from '../../services/data.service';
+import { Observable, forkJoin, switchMap, map } from 'rxjs';
+import { CommonModule } from '@angular/common';
+import { Router, RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-post-list',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.scss'],
+})
+export class PostListComponent implements OnInit {
+  posts$!: Observable<Data[]>;
+  postsWithComments$!: Observable<{ post: Data; commentsCount: number }[]>;
+
+  constructor(
+    private dataService: DataService,
+    private commentService: CommentService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    // Fetch posts
+    this.posts$ = this.dataService.getAllPosts();
+
+    // Fetch posts along with corresponding comments count
+    this.postsWithComments$ = this.posts$.pipe(
+      switchMap((posts: Data[]) => {
+        const postWithCommentObservables = posts.map((post: Data) =>
+          this.commentService.getCommentByPostId(post.id).pipe(
+            map((comments) => ({
+              post,
+              commentsCount: comments.length,
+            }))
+          )
+        );
+        return forkJoin(postWithCommentObservables);
+      })
+    );
+  }
+
+  // Method to navigate to the post details page
+  viewPostDetails(postId: number): void {
+    console.log("Navigating to post Id", postId);
+    this.router.navigate(['/post-details', postId]);
+  }
+}
