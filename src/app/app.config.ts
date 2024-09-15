6d67a1bfdce7d44348aae0b8b1f347492459eb51
Eric-Obeng <obengeric.3958@gmail.com>
2024-09-15 21:41:41 +0000

    app config updated to import CacheInterceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,9 +2,29 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import {
+  provideHttpClient,
+  withFetch,
+  withInterceptors,
+  HTTP_INTERCEPTORS,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
 import { httpInterceptorFn } from './services/http.interceptor';
+import { CacheInterceptor } from './interceptors/cache-interceptor.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient(withFetch(), withInterceptors([httpInterceptorFn]))],
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(
+      withFetch(),
+      withInterceptors([httpInterceptorFn]),
+      withInterceptorsFromDi()
+    ),
+
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: CacheInterceptor,
+      multi: true,
+    },
+  ],
 };
