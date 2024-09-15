597e437ff998ec53342d7c74aba4f2053472662d
Eric-Obeng <obengeric.3958@gmail.com>
2024-09-15 19:10:57 +0000

    post list updated to add pagination

--- src/app/components/post-list/post-list.component.ts
+++ src/app/components/post-list/post-list.component.ts
@@ -1,46 +1,60 @@
 import { Component, OnInit } from '@angular/core';
 import { Data } from '../../interfaces/data';
-import { CommentService } from '../../services/comment.service';
 import { DataService } from '../../services/data.service';
-import { Observable, forkJoin, switchMap, map } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 import { CommonModule } from '@angular/common';
-import { Router, RouterModule } from '@angular/router';
-import { PostCreateComponent } from "../post-create/post-create.component";
+import { RouterModule } from '@angular/router';
+import { PostCreateComponent } from '../post-create/post-create.component';
+import { PaginationComponent } from '../pagination/pagination.component';
 
 @Component({
   selector: 'app-post-list',
   standalone: true,
-  imports: [CommonModule, RouterModule, PostCreateComponent],
+  imports: [
+    CommonModule,
+    RouterModule,
+    PostCreateComponent,
+    PaginationComponent,
+  ],
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.scss'],
 })
 export class PostListComponent implements OnInit {
-  posts$!: Observable<Data[]>;
-  postsWithComments$!: Observable<{ post: Data; commentsCount: number }[]>;
+  posts$!: Observable<Data[]>; // Only the posts for the current page
+  currentPage: number = 1;
+  totalPages: number = 1;
+  postsPerPage: number = 10;
 
-  constructor(
-    private dataService: DataService,
-    private commentService: CommentService,
-    private router: Router
-  ) {}
+  constructor(private dataService: DataService) {}
 
   ngOnInit(): void {
-    // Fetch posts
-    this.posts$ = this.dataService.getAllPosts();
+    this.loadPosts();
+  }
 
-    // Fetch posts along with corresponding comments count
-    this.postsWithComments$ = this.posts$.pipe(
-      switchMap((posts: Data[]) => {
-        const postWithCommentObservables = posts.map((post: Data) =>
-          this.commentService.getCommentByPostId(post.id).pipe(
-            map((comments) => ({
-              post,
-              commentsCount: comments.length,
-            }))
-          )
-        );
-        return forkJoin(postWithCommentObservables);
-      })
-    );
+  loadPosts(): void {
+    // Fetch posts for the current page and update localStorage
+    this.posts$ = this.dataService
+      .getAllPosts(this.currentPage, this.postsPerPage)
+      .pipe(
+        tap((posts) => {
+          // Clear previous local storage data
+          localStorage.removeItem('currentPosts');
+
+          // Update localStorage with current page posts
+          localStorage.setItem('currentPosts', JSON.stringify(posts));
+
+          // Update total pages assuming 100 total posts (or fetch total count from API)
+          this.totalPages = Math.ceil(100 / this.postsPerPage);
+
+          console.log('Loaded posts for page', this.currentPage, posts);
+        })
+      );
+  }
+
+  onPageChange(newPage: number): void {
+    if (newPage >= 1 && newPage <= this.totalPages) {
+      this.currentPage = newPage;
+      this.loadPosts(); // Load new posts for the current page
+    }
   }
 }
