af1e96325d3629aefbb3480a6f53f16c574cb14c
Eric-Obeng <obengeric.3958@gmail.com>
2024-09-15 00:06:29 +0000

    added create post component

--- src/app/components/post-details/post-details.component.ts
+++ src/app/components/post-details/post-details.component.ts
@@ -1,12 +1,78 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { DataService } from '../../services/data.service';
+import { CommentService } from '../../services/comment.service';
+import { Observable, of, switchMap } from 'rxjs';
+import { Comment, Data } from '../../interfaces/data';
+import { CommonModule } from '@angular/common';
+import { FormsComponent } from "../forms/forms.component";
 
 @Component({
   selector: 'app-post-details',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsComponent],
   templateUrl: './post-details.component.html',
-  styleUrl: './post-details.component.scss'
+  styleUrl: './post-details.component.scss',
 })
 export class PostDetailsComponent {
+  post$!: Observable<Data | null>;
+  comments$!: Observable<Comment[]>;
+  currentPost!: Data;
 
+  showFormModal: boolean = false;
+
+  constructor(
+    private postService: DataService,
+    private commentService: CommentService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    const postId = this.route.snapshot.paramMap.get('id');
+
+    this.post$ = this.route.paramMap.pipe(
+      switchMap((params) => {
+        const postId = Number(params.get('id'));
+        console.log('Routed post ID:', postId); // Debugging: Log the routed post ID
+        return this.postService.getPostByUserId(postId).pipe(
+          switchMap((posts) => {
+            console.log('Fetched post data:', posts); // Debugging: Log the fetched posts
+            return posts.length ? of(posts[0]) : of(null);
+          })
+        );
+      })
+    );
+
+    // Fetch comments for the post
+    this.route.paramMap
+      .pipe(
+        switchMap((params) => {
+          const postId = Number(params.get('id'));
+          return this.commentService.getCommentByPostId(postId);
+        })
+      )
+      .subscribe({
+        next: (comments) => (this.comments$ = of(comments)),
+      });
+
+    this.post$.subscribe((post) => {
+      if (post) {
+        this.currentPost = post;
+      }
+    });
+  }
+
+  openPostForm() {
+    this.showFormModal = true;
+  }
+
+  handleForEdit(post: Data) {
+    this.postService.editPost(post).subscribe({
+      next: () => {
+        this.showFormModal = false;
+        this.router.navigate(['/posts']);
+      },
+    });
+  }
 }
