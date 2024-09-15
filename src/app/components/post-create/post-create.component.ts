a11b9317f5904c85498d28fb9d8ef7d121993b9c
Eric-Obeng <obengeric.3958@gmail.com>
2024-09-15 00:06:06 +0000

    feat: post create component to show form and also to create new post

--- src/app/components/post-create/post-create.component.ts
+++ src/app/components/post-create/post-create.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { DataService } from '../../services/data.service';
+import { Router } from '@angular/router';
+import { Data } from '../../interfaces/data';
+import { FormsComponent } from "../forms/forms.component";
 
 @Component({
   selector: 'app-post-create',
   standalone: true,
-  imports: [],
+  imports: [FormsComponent],
   templateUrl: './post-create.component.html',
-  styleUrl: './post-create.component.scss'
+  styleUrl: './post-create.component.scss',
 })
 export class PostCreateComponent {
+  showFormModal: boolean = false;
 
+  constructor(private postService: DataService, private router: Router) {}
+
+  openPostForm() {
+    this.showFormModal = true;
+  }
+
+  handleForSubmit(post: Data) {
+    this.postService.createPost(post).subscribe({
+      next: () => {
+        this.router.navigate(['/posts']);
+        this.showFormModal = false;
+      },
+      error: (err) => {
+        console.error('Error creating post', err);
+      },
+    });
+  }
 }
