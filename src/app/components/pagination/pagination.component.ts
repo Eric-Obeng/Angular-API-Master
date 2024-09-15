7d0c828ab047b502fc7869900c4e0c6952b48ea6
Eric-Obeng <obengeric.3958@gmail.com>
2024-09-15 19:09:25 +0000

    Added pagination

--- src/app/components/pagination/pagination.component.ts
+++ src/app/components/pagination/pagination.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-pagination',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './pagination.component.html',
-  styleUrl: './pagination.component.scss'
+  styleUrl: './pagination.component.scss',
 })
 export class PaginationComponent {
+  @Input() currentPage: number = 1;
+  @Input() totalPages!: number;
+  @Output() pageChange = new EventEmitter<number>();
 
+  changePage(newPage: number): void {
+    if (newPage >= 1 && newPage <= this.totalPages) {
+      this.pageChange.emit(newPage);
+    }
+  }
 }
