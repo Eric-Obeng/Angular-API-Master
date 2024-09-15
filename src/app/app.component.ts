5288bd2a756df5684128a7936d2b73d8035606b8
Eric-Obeng <obengeric.3958@gmail.com>
2024-09-15 00:07:11 +0000

    pdated app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PostCreateComponent } from "./components/post-create/post-create.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PostCreateComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
